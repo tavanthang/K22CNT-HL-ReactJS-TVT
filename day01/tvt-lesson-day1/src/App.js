7df315534d05a8ab27824a0940d79b01657fdb7d
Tạ Văn Thắng <youremail@example.com>
2025-09-03 22:44:33 +0700

    First commit

---
+++ day01/tvt-lesson-day1/src/App.js
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="student-info">
+      <h1>Thông tin sinh viên</h1>
+      <p>Họ và tên: Tạ Văn Thắng</p>
+      <p>Mã sinh viên: 2210900120</p>
+      <p>Ngày sinh: 10/02/2004</p>
+      <p>Lớp: CNTT K22</p>
+      <p>Khoa: Công nghệ Thông tin</p>
+      <p>Ngành học: Công nghệ Thông tin</p>
+      <p>Email: tavanthang2004@example.com</p>
+      <p>Số điện thoại: 0123 456 789</p>
+      <p>Địa chỉ: Hà Nội, Việt Nam</p>
+      <a href="https://www.facebook.com/tavanthang2004" target="_blank" rel="noopener noreferrer">
+        Facebook cá nhân
+      </a>
+    </div>
+  );
+}
+
+export default App;
